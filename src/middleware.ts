e3daf481916365d2965db0340611e495b5ac38a7
rezkaudi <audirezk@gmail.com>
2025-04-25 17:43:51 +0300

    try to fix session issue

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,20 +5,24 @@ export async function middleware(request: NextRequest) {
     const { pathname } = request.nextUrl;
     const PUBLIC_PATHS = ['/login', '/register', '/verify', '/forget-password', '/reset-password', '/privacy-policy'];
 
-    // Allow access to home page without authentication
-    if (PUBLIC_PATHS.includes(pathname)) return NextResponse.next();
-
     // Get the token from cookies
     const token = request.cookies.get('accessToken');
+
+    console.log("token", token)
+    if (token && pathname === "/login") {
+        console.log("7777")
+        return NextResponse.redirect(new URL('/', request.url));
+    }
+
+    // Allow access to home page without authentication
+    if (PUBLIC_PATHS.includes(pathname)) return NextResponse.next();
 
     // If no token found, redirect to login
     if (!token) {
         return NextResponse.redirect(new URL('/login', request.url));
     }
 
-    if (token && pathname === "/login") {
-        return NextResponse.redirect(new URL('/', request.url));
-    }
+
 
     // Token exists, allow access to protected pages
     return NextResponse.next();
