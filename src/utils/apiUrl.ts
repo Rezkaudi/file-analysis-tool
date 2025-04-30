7c8a54c021fd0e9d550bb58338577c1095c3151f
rezkaudi <audirezk@gmail.com>
2025-04-30 15:31:55 +0300

    fix cookie issue and add apiurl in .env

--- src/utils/apiUrl.ts
+++ src/utils/apiUrl.ts
@@ -1,1 +1,2 @@
-export const apiUrl: string = "https://zero-production-2bcd.up.railway.app/api"
+// export const apiUrl: string = "https://zero-production-2bcd.up.railway.app/api"
+export const apiUrl = process.env.NEXT_PUBLIC_API_URL
