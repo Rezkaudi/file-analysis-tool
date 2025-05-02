5c36c7b579f44a754755079da045456e4fadbfad
rezkaudi <audirezk@gmail.com>
2025-05-02 15:19:24 +0300

    fix redirect issue after payment

--- src/app/position/[id]/page.tsx
+++ src/app/position/[id]/page.tsx
@@ -1,29 +1,29 @@
-'use client';
+// 'use client';
 
-import { useParams } from 'next/navigation';
-import Analysis from "@/components/pages/analysis";
+// import { useParams } from 'next/navigation';
+// import Analysis from "@/components/pages/analysis";
 
-export default function AnalysisPage() {
-  const params = useParams();
-  const id = params.id as string;
-
-  return <Analysis id={id} />;
-}
-
-// // page
-// import Analysis from "@/components/pages/analysis"
-
-// interface IAnalysis {
-//   params: Promise<{
-//     id: string;
-//   }>;
-// }
-
-// export default async function AnalysisPage({ params }: IAnalysis) {
-//   const { id } = await params;
+// export default function AnalysisPage() {
+//   const params = useParams();
+//   const id = params.id as string;
 
 //   return <Analysis id={id} />;
 // }
+
+// page
+import Analysis from "@/components/pages/analysis"
+
+interface IAnalysis {
+  params: Promise<{
+    id: string;
+  }>;
+}
+
+export default async function AnalysisPage({ params }: IAnalysis) {
+  const { id } = await params;
+
+  return <Analysis id={id} />;
+}
 
 
 
